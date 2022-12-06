b220d0b7379f6abb7e346abf19f7fb2b1b728aa9
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2022-12-06 15:18:40 +0530

    gh-pages update

---
+++ assets/AssessmentAttempts.87d98e31.js
@@ -0,0 +1,1 @@
+import{bH as t,bb as e,bk as a}from"./vendor.5ecbfc3b.js";import"./vendor_core_js_pure.42317256.js";const n=()=>{const{assessmentId:s}=t();return e(a,{children:["Hi ",s]})};export{n as default};
